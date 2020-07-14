4b47471cc15d87d6062b08bd9c854cb5fb027fbd
Pedro Giacomin <pedrogiacomin2706@gmail.com>
2020-07-14 14:24:09 -0300

    Piece movecount

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -98,9 +98,11 @@ public class ChessMatch {
 	 e guarda em capturedPiece, posiciona p em target
 	 */
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece) board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
+		
 		if(capturedPiece != null) {
 			piecesOnTheBoard.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
@@ -110,7 +112,8 @@ public class ChessMatch {
 	
 	//Desfaz o movimento, o inverso do makeMove (usado na lógica de xeque)
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		if(capturedPiece != null) {
 			board.placePiece(capturedPiece, target);
